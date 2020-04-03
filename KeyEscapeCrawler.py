c5c9e9f6626761f934152f9721776d3f7544141d
JunHyeokHa <sirl0@naver.com>
2020-04-03 23:44:51 +0900

    fix: 키이스케이프 데이터가 이전 날짜와 덮어씌워지는 오류 수정

--- KeyEscapeCrawler.py
+++ KeyEscapeCrawler.py
@@ -13,36 +13,38 @@ driver.get("https://keyescape.co.kr/web/home.php?go=rev.make")
 point = input()
 theme = input()
 
+
 # 지점 설정
 def set_point(input):
-   point = driver.find_element_by_id('zizum_data').find_element_by_partial_link_text(input)
-   point.click()
+  point = driver.find_element_by_id('zizum_data').find_element_by_partial_link_text(input)
+  point.click()
 
 
 # 날짜 설정
 def set_date(input):
-   dates = driver.find_element_by_class_name('t_calendar').find_elements_by_tag_name('a')
-   for date in dates:
-      print(date.text + '일')
-      date.send_keys(Keys.ENTER)
-      time.sleep(1)
-      set_theme(input)
-      time.sleep(1)
+  dates = driver.find_element_by_class_name('t_calendar').find_elements_by_tag_name('a')
+  for date in dates:
+    print(date.text + '일')
+    date.send_keys(Keys.ENTER)
+    time.sleep(1)
+    set_theme(input)
+    time.sleep(1)
 
 
 # 테마 설정
 def set_theme(input):
-   theme = driver.find_element_by_id('theme_data').find_element_by_link_text(input)
-   theme.click()
-   time.sleep(1)
-   set_time()
+  theme = driver.find_element_by_id('theme_data').find_element_by_link_text(input)
+  theme.click()
+  time.sleep(2)
+  set_time()
+
 
 # 시간 설정
 def set_time():
-   time = driver.find_element_by_id('theme_time_data').find_elements_by_class_name('possible')
-   print(str(len(time))+'개 가능')
-   for item in time:
-      print(item.text)
+  time = driver.find_element_by_id('theme_time_data').find_elements_by_class_name('possible')
+  print(str(len(time)) + '개 가능')
+  for item in time:
+    print(item.text)
 
 
 set_point(point)
